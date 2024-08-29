3fa09744c344354a99f09cf99e2262616898a8c2
gyorfymate <gyorfy.mate@gmail.com>
2024-08-29 17:18:50 +0200

    alles gut

---
+++ src/i18n/ui.ts
@@ -0,0 +1,87 @@
+export const languages = {
+  en: "English",
+  hu: "Magyar",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    appname: "Brendo",
+
+    "nav.home": "Home",
+    "nav.explore": "Explore",
+    "nav.download": "Download App",
+    "nav.contactus": "Contact Us",
+
+    "hero.title": "Discover Fun Activities Waiting For You",
+    "hero.subtitle_1": "Let",
+    "hero.subtitle_2": "help you find program ideas  and spend quality offline time with your companions, whether you're a local or a tourist",
+    "hero.button": "Get app",
+
+    "features.title.explore": "Explore",
+    "features.title.connect": "Connect",
+    "features.title.memories": "Make memories",
+    "features.title.shareidea": "Share your ideas",
+    "features.title.tourist": "Make it easy for tourist",
+
+    "features.desc.explore": "Explore our continuously growing collection of program ideas contributed by our community members. There are ideas for everyone!",
+    "features.desc.connect":
+      "Make planning easier by creating groups with your partner or friends to keep a shared list of the activities you want go to together.",
+    "features.desc.memories":
+      "Have a beautiful list of all the activities you’ve been to with your loved one. Create albums and remember your best times together!",
+    "features.desc.shareidea":
+      "You’ve got a program idea that most of us don’t know about? Share your idea with our members to help them enjoy a fantastic time together!",
+    "features.desc.tourist":
+      "You can turn in Tourist mode to display all the ideas in English instead of the native language making it easier for you to explore abroad",
+
+    "download.title": "Download",
+    "donwload.subtitle": "Brendo is available for free on both iOS and Android. You can find them their corresponding app stores or use the button below",
+
+    "footer.navigation": "Navigation",
+    "footer.documents": "Documents",
+
+    "document.privacypolicy": "Privacy Policy",
+    "document.tac": "Terms & Conditions",
+  },
+  hu: {
+    appname: "Brendo",
+
+    "nav.home": "Kezdés",
+    "nav.explore": "Lehetőségek",
+    "nav.download": "Letöltés",
+    "nav.contactus": "Kapcsolat",
+
+    "hero.title": "Fedezd fel a körülötted rejlő programokat!",
+    "hero.subtitle_1": "Használd a ",
+    "hero.subtitle_2": "mobil alkalmazást hogy új, izgalmas szabadidős programokat találj és minőségi időt tölthess társaiddal akár helyi, akár turista vagy",
+    "hero.button": "Letöltés",
+
+    "features.title.explore": "Fedezz fel ötleteket!",
+    "features.title.connect": "Kapcsolódj!",
+    "features.title.memories": "Gyűjts elmékeket!",
+    "features.title.shareidea": "Oszd meg ötleteid!",
+    "features.title.tourist": "Légy turista!",
+
+    "features.desc.explore":
+      "Fedezd fel a folyamatosan bővülő programjaink gyűjteményét, amelyeket a közösségünk tagjai töltöttek fel. Mindenki számára találunk ötletet!",
+    "features.desc.connect":
+      "Tedd könnyebbé a szervezést csoportok létrehozásával. Hozzatok létre egy közös listát azokkal a program ötletekkel, amelyeken szívesen részt vennétek közösen!",
+    "features.desc.memories":
+      "Szeretnél egy gyűjteményt azokról a programokról, amikre a pároddal vagy barátaiddal elmentél? Hozz létre emlékeket és add hozzá a közös albumhoz, hogy elékezz a legjobb közös élményekre!",
+    "features.desc.shareidea":
+      "Van egy jó program ötleted, amit a sokan nem ismernek? Oszd meg azt a többi felhasználóval, hogy segíts nekik kellemes emlékeket gyűjteni!",
+    "features.desc.tourist":
+      "A Turista mód segítségével külföldön is könnyű lesz felfedezni, hiszen a programokat lefordítjuk neked angolra, így nem kell beszélned helyi nyelvet.",
+
+    "download.title": "Letöltés",
+    "donwload.subtitle":
+      "Brendo alkalmazás ingyenesen elérhető iPhone és Android készülékekre egyaránt. Letöltheted az App Store, illetve a Google Play áruházakból!",
+
+    "footer.navigation": "Navigáció",
+    "footer.documents": "Dokumentumok",
+
+    "document.privacypolicy": "Adatvédelmi nyilatkozat",
+    "document.tac": "ÁSZF",
+  },
+} as const;
