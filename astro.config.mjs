debe1b2976c303af5778eb4814677f4b333b7eb9
gyorfymate <gyorfy.mate@gmail.com>
2024-09-03 18:35:51 +0200

    for github deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mategyorfy.github.io",
+  base: "/brendo-website",
   integrations: [tailwind(), icon()]
 });
