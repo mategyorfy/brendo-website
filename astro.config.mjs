eb0fcc053e0bae126b2771e64b47d3afc4dca45f
gyorfymate <gyorfy.mate@gmail.com>
2024-09-26 20:32:16 +0200

    site base added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-/*   site: 'https://mategyorfy.github.io',
-  base: 'brendo-website', */
-  integrations: [tailwind(), icon()]
+  site: "https://mategyorfy.github.io",
+  base: "brendo-website",
+  integrations: [tailwind(), icon()],
 });
