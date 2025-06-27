5c66e51fb7a3654d4bb1a292a206a0ffe92be9de
gyorfymate <gyorfy.mate@gmail.com>
2025-06-28 00:05:05 +0200

    Update some texts

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -20,23 +20,26 @@ export const ui = {
     "hero.button": "Get app",
 
     "features.title.explore": "Explore",
-    "features.title.connect": "Connect",
+    //"features.title.connect": "Connect",
+    "features.title.connect": "Save your favourites",
     "features.title.memories": "Make memories",
     "features.title.shareidea": "Share your ideas",
     "features.title.tourist": "Make it easy for tourist",
 
     "features.desc.explore": "Explore our continuously growing collection of program ideas contributed by our community members. There are ideas for everyone!",
+    //"features.desc.connect":
+    //  "Make planning easier by creating groups with your partner or friends to keep a shared list of the activities you want go to together.",
     "features.desc.connect":
-      "Make planning easier by creating groups with your partner or friends to keep a shared list of the activities you want go to together.",
+      "Make deciding where to go quicker by saving the ideas you like to your favourites — you'll always have one on hand!",
     "features.desc.memories":
-      "Have a beautiful list of all the activities you’ve been to with your loved one. Create albums and remember your best times together!",
+      "Have a beautiful list of all the activities you’ve been to with your companions. Create polariod like photos to remember your best times together!",
     "features.desc.shareidea":
       "You’ve got a program idea that most of us don’t know about? Share your idea with our members to help them enjoy a fantastic time together!",
     "features.desc.tourist":
       "You can turn in Tourist mode to display all the ideas in English instead of the native language making it easier for you to explore abroad",
 
-    "download.title": "Download",
-    "donwload.subtitle": "Brendo is available for free on both iOS and Android. You can find them their corresponding app stores or use the button below",
+    "download.title": "Download Brendo",
+    "donwload.subtitle": "Brendo is available for free on both iOS and Android. You can find them their corresponding app stores or use the buttons below",
 
     "footer.navigation": "Navigation",
     "footer.documents": "Documents",
@@ -52,29 +55,32 @@ export const ui = {
     "nav.download": "Letöltés",
     "nav.contactus": "Kapcsolat",
 
-    "hero.title": "Fedezd fel a körülötted rejlő programokat!",
+    "hero.title": "Fedezd fel Magyarországot és a körülötted rejlő programokat!",
     "hero.subtitle_1": "Használd a ",
     "hero.subtitle_2": "mobil alkalmazást hogy új, izgalmas szabadidős programokat találj és minőségi időt tölthess társaiddal akár helyi, akár turista vagy",
     "hero.button": "Letöltés",
 
     "features.title.explore": "Fedezz fel ötleteket!",
-    "features.title.connect": "Kapcsolódj!",
+   // "features.title.connect": "Kapcsolódj!",
+    "features.title.connect": "Mentd el kedvenceid!",
     "features.title.memories": "Gyűjts elmékeket!",
     "features.title.shareidea": "Oszd meg ötleteid!",
     "features.title.tourist": "Légy turista!",
 
     "features.desc.explore":
       "Fedezd fel a folyamatosan bővülő programjaink gyűjteményét, amelyeket a közösségünk tagjai töltöttek fel. Mindenki számára találunk ötletet!",
+ //   "features.desc.connect":
+ //     "Tedd könnyebbé a szervezést csoportok létrehozásával. Hozzatok létre egy közös listát azokkal a program ötletekkel, amelyeken szívesen részt vennétek közösen!",
     "features.desc.connect":
-      "Tedd könnyebbé a szervezést csoportok létrehozásával. Hozzatok létre egy közös listát azokkal a program ötletekkel, amelyeken szívesen részt vennétek közösen!",
+      "Tedd gyorsabbá az ötletkeresést azzal, hogy elmented a kedvenceid közé, ami tetszik – így mindig kéznél lesz egy!",
     "features.desc.memories":
-      "Szeretnél egy gyűjteményt azokról a programokról, amikre a pároddal vagy barátaiddal elmentél? Hozz létre emlékeket és add hozzá a közös albumhoz, hogy elékezz a legjobb közös élményekre!",
+      "Készíts polariodokat emlékeidről és akaszd ki a galériádba, hogy bármikor visszanézhesd kedvenceid!",
     "features.desc.shareidea":
       "Van egy jó program ötleted, amit a sokan nem ismernek? Oszd meg azt a többi felhasználóval, hogy segíts nekik kellemes emlékeket gyűjteni!",
     "features.desc.tourist":
       "A Turista mód segítségével külföldön is könnyű lesz felfedezni, hiszen a programokat lefordítjuk neked angolra, így nem kell beszélned helyi nyelvet.",
 
-    "download.title": "Letöltés",
+    "download.title": "Brendo letöltése",
     "donwload.subtitle":
       "Brendo alkalmazás ingyenesen elérhető iPhone és Android készülékekre egyaránt. Letöltheted az App Store, illetve a Google Play áruházakból!",
 
